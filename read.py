8fd67e756df76ee31e91f40a23c7627866dd2d48
icebear <icebearliangliang@163.com>
2019-04-13 19:44:27 +0800

    average

---
+++ read.py
@@ -0,0 +1,17 @@
+data = []
+count = 0
+sum1 = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('档案读取完了，总共有', len(data), '笔资料')
+
+
+for d in data:
+	length = len(d)
+	sum1 = sum1 + length
+average = sum1 / len(data)
+print(average)
